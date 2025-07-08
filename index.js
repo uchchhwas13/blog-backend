110e26121dceb437a326e2242a876400b0c77534
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-07-08 19:32:06 +0600

    Adding nodemon package as devDependency

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ const PORT = 3000;
 app.set('view engine', 'ejs');
 app.set('views', path.resolve('./views'));
 
+app.route('/', (req, res) => {
+    res.render('home');
+});
+
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
