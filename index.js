9fd632284a2cf073064d072603e53afaa9596a1a
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-07-08 19:04:23 +0600

    setting view engine and views path

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.resolve('./views'));
+
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
