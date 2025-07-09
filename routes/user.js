48666b72b230a1fd393a8760b4b5aa4bc5fe25a0
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-07-09 12:25:15 +0600

    Adding route for sign-in post request

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,4 @@
-const {Router} = require('express');
+const { Router } = require('express');
 const User = require('../models/user');
 
 const router = Router();
@@ -11,12 +11,23 @@ router.get('/signup', (req, res) => {
   return res.render('signup');
 });
 
+router.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).send('All fields are required');
+  }
+  
+  const user = await User.matchPassword(email, password);
+  console.log('User found:', user);
+  return res.redirect('/');
+});
+
 router.post('/signup', async (req, res) => {
-  const {fullname, email, password} = req.body;
-  console.log('Request body',req.body);
+  const { fullname, email, password } = req.body;
+  console.log('Request body', req.body);
   if (!fullname || !email || !password) {
-  return res.status(400).send('All fields are required');
-}
+    return res.status(400).send('All fields are required');
+  }
 
   await User.create({
     name: fullname,
