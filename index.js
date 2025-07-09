3ab817a6b5d28052d0cda0b455f3bdc3fd95c9d1
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-07-09 14:47:46 +0600

    Adding cookiePareser and checkAuthenticationCookie for all routes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 
 const userRouter = require('./routes/user');
+const { checkAuthenticationCookie } = require('./middlewares/authentication');
 
 const app = express();
 const PORT = 3000;
@@ -11,13 +13,17 @@ mongoose.connect('mongodb://localhost:27017/blogify')
     .then(() => console.log('Connected to MongoDB'));
 
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(checkAuthenticationCookie('token'));
+
 app.set('view engine', 'ejs');
 app.set('views', path.resolve('./views'));
 
 
-
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', {
+        user: req.user 
+    });
 });
 
 app.use("/user", userRouter);
