9f4dbf0ab533e345c90243d24082bf6875724379
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-08-13 21:50:13 +0600

    redirecting to homepage after successful login

--- src/controllers/signin.controller.ts
+++ src/controllers/signin.controller.ts
@@ -10,21 +10,10 @@ export const handleSignin = async (
 
   try {
     const token = await User.matchPasswordAndGenerateToken(email, password);
-
-    res
-      .cookie('token', token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
-        maxAge: 24 * 60 * 60 * 1000,
-      })
-      .status(200)
-      .json({
-        success: true,
-        message: 'Login successful',
-      });
+    console.log('Generated token:', token);
+    return res.cookie('token', token).redirect('/');
   } catch (error) {
-    res.status(401).json({
+    return res.status(401).json({
       success: false,
       message: 'Incorrect email or password',
     });
