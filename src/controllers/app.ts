eca26ce999a489b6601f6eb9d2f661a9836c78c5
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-08-13 11:45:33 +0600

    Migrating app controller into ts

---
+++ src/controllers/app.ts
@@ -0,0 +1,13 @@
+import { Blog } from '../models/blog';
+import { User } from '../models/user';
+import { Request, Response } from 'express';
+
+export const renderHomePage = async (req: Request, res: Response) => {
+  const blogs = await Blog.find({});
+  //const user = req.user ? await User.findOne({ email: req.user.email }) : null;
+  //console.log('User info: ', user);
+  res.render('home', {
+    //user: user,
+    blogs: blogs,
+  });
+};
