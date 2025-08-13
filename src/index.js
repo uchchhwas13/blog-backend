7639ffded143ad9c956f3d20a56670e238a3ef1d
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-08-13 11:24:10 +0600

    Commenting index.js file

--- src/index.js
+++ src/index.js
@@ -1,48 +1,48 @@
 //This file contains the same code as the app.js. I will drop this file later.
 
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
-const Blog = require('./models/blog');
-const User = require('./models/user');
+// const express = require('express');
+// const path = require('path');
+// const mongoose = require('mongoose');
+// const cookieParser = require('cookie-parser');
+// const Blog = require('./models/blog');
+// const User = require('./models/user');
 
-const userRouter = require('./routes/user');
-const blogRouter = require('./routes/blog');
+// const userRouter = require('./routes/user.route');
+// const blogRouter = require('./routes/blog');
 
-const { checkAuthenticationCookie } = require('./middlewares/authentication');
+// const { checkAuthenticationCookie } = require('./middlewares/authentication');
 
-const app = express();
-const PORT = 3000;
+// const app = express();
+// const PORT = 3000;
 
-mongoose
-  .connect('mongodb://localhost:27017/blogify')
-  .then(() => console.log('Connected to MongoDB'));
+// mongoose
+//   .connect('mongodb://localhost:27017/blogify')
+//   .then(() => console.log('Connected to MongoDB'));
 
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(checkAuthenticationCookie('token'));
-app.use(express.static(path.resolve('./public')));
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(checkAuthenticationCookie('token'));
+// app.use(express.static(path.resolve('./public')));
 
-app.set('view engine', 'ejs');
-app.set('views', path.resolve('./views'));
+// app.set('view engine', 'ejs');
+// app.set('views', path.resolve('./views'));
 
-app.get('/', async (req, res) => {
-  const blogs = await Blog.find({});
-  let userInfo;
-  if (req.user) {
-    userInfo = await User.findOne({ email: req.user.email });
-  } else {
-    userInfo = null;
-  }
-  console.log('User info: ', userInfo);
-  res.render('home', {
-    user: userInfo,
-    blogs: blogs,
-  });
-});
+// app.get('/', async (req, res) => {
+//   const blogs = await Blog.find({});
+//   let userInfo;
+//   if (req.user) {
+//     userInfo = await User.findOne({ email: req.user.email });
+//   } else {
+//     userInfo = null;
+//   }
+//   console.log('User info: ', userInfo);
+//   res.render('home', {
+//     user: userInfo,
+//     blogs: blogs,
+//   });
+// });
 
-app.use('/user', userRouter);
-app.use('/blog', blogRouter);
+// app.use('/user', userRouter);
+// app.use('/blog', blogRouter);
 
-app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+// app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
