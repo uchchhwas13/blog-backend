b8efa570dd036d651ba88c104fcea4c9fd77e28e
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-08-14 08:57:25 +0600

    Passing logged in user in response

--- src/controllers/app.ts
+++ src/controllers/app.ts
@@ -4,10 +4,10 @@ import { Request, Response } from 'express';
 
 export const renderHomePage = async (req: Request, res: Response) => {
   const blogs = await Blog.find({});
-  //const user = req.user ? await User.findOne({ email: req.user.email }) : null;
-  //console.log('User info: ', user);
+  const user = req.user ? await User.findOne({ email: req.user.email }) : null;
+  console.log('User info: ', user);
   res.render('home', {
-    //user: user,
+    user: user,
     blogs: blogs,
   });
 };
