fa7784b26ee5cd0614619da9badc9853d57864b3
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-08-15 16:34:01 +0600

    Dropping commented code

--- src/routes/blog.route.ts
+++ src/routes/blog.route.ts
@@ -43,35 +43,3 @@ router.get('/:id', handleGetBlogDetails);
 router.post('/comment/:blogId', authenticateRequest('accessToken'), handleAddComment);
 
 export default router;
-
-// const storage = multer.diskStorage({
-//   destination: function (req: Request, file, cb) {
-//     cb(null, path.resolve(`./public/uploads/`));
-//   },
-//   filename: function (req, file, cb) {
-//     const fileName = `${Date.now()}-${file.originalname}`;
-//     cb(null, fileName);
-//   },
-// });
-
-// const upload = multer({ storage: storage });
-
-// router.get('/add-new', (req, res) => {
-//   if (!req.user) {
-//     return res.redirect('/user/signin');
-//   }
-
-//   return res.render('addBlog', {
-//     user: req.user,
-//   });
-// });
-
-// router.get('/add-new', (req: AuthRequest, res: Response) => {
-//   if (!req.user) {
-//     return res.redirect('/user/signin');
-//   }
-
-//   return res.render('addBlog', {
-//     user: req.user,
-//   });
-// });
