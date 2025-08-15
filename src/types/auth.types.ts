ef96989dfef5c0410ea726fcc215b83a89a6f104
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-08-15 12:53:03 +0600

    Adding user data type

--- src/types/auth.types.ts
+++ src/types/auth.types.ts
@@ -6,11 +6,23 @@ export type AuthPayload = {
 export type SigninSuccessResponse = {
   success: true;
   message: string;
+  data: {
+    user: UserData;
+    accessToken: string;
+    refreshToken: string;
+  };
 };
 
 export type SigninErrorResponse = {
   success: false;
   message: string;
+};
+
+type UserData = {
+  _id: string;
+  email: string;
+  name: string;
+  role: string;
 };
 
 export type SigninResponse = SigninSuccessResponse | SigninErrorResponse;
