61b1244aadea8f2360497b4dd19c691a6d2d8253
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-08-15 19:21:43 +0600

    Added route for refresh-access-token

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,6 +1,10 @@
 import { Router } from 'express';
 import { handleSignup } from '../controllers/signup.controller';
-import { handleSignin, logoutUser } from '../controllers/signin.controller';
+import {
+  handleRefreshAccessToken,
+  handleSignin,
+  logoutUser,
+} from '../controllers/signin.controller';
 import { validateBody } from '../middlewares/validateBlog.middleware';
 import { signupSchema } from '../validations/signupSchema';
 import { signinSchema } from '../validations/signinSchema';
@@ -11,6 +15,7 @@ userRouter.post('/signin', validateBody(signinSchema), handleSignin);
 userRouter.post('/signup', validateBody(signupSchema), handleSignup);
 
 userRouter.post('/logout', logoutUser);
+userRouter.post('/refresh-access-token', handleRefreshAccessToken);
 
 export default userRouter;
 // router.post('/signin', async (req, res) => {
