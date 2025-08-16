8d6ee15c52b82bcdd0f6e054fa6b0a6bc2cb2b27
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-08-16 11:17:12 +0600

    Adding errorHandler

---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from 'express';
+import { ApiError } from '../utils/ApiError';
+
+export function errorHandler(err: unknown, req: Request, res: Response, next: NextFunction) {
+  //console.error(err);
+
+  if (err instanceof ApiError) {
+    // Custom API errors
+    return res.status(err.statusCode).json({
+      success: err.success,
+      message: err.message,
+      errors: err.errors || [],
+    });
+  }
+
+  // Fallback for unknown errors
+  return res.status(500).json({
+    success: false,
+    message: (err as Error).message || 'Internal Server Error',
+  });
+}
