78d346af5c5782d17715cd43f709444af91fa652
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-08-25 09:56:13 +0600

    dropping commented code

--- src/models/user.ts
+++ src/models/user.ts
@@ -103,21 +103,3 @@ userSchema.methods.generateRefreshToken = function (): string {
 };
 
 export const User = mongoose.model<IUser>('User', userSchema);
-
-// userSchema.static(
-//   'matchPasswordAndGenerateToken',
-//   async function (email, password): Promise<string> {
-//     console.log('Matching password for email:', email);
-//     const user = await this.findOne({ email });
-//     if (!user) throw new Error('User not found');
-
-//     const salt = user.salt;
-//     const hashedPassword = user.password;
-//     const userProvidedHash = createHmac('sha256', salt).update(password).digest('hex');
-
-//     if (userProvidedHash !== hashedPassword) throw new Error('Incorrect password');
-
-//     const token = generateTokenForUser(user);
-//     return token;
-//   },
-// );
