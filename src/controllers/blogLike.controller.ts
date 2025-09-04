edfa9f9f5e4b79a4bfaf23e841f3073121bd4543
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-09-04 15:14:40 +0600

    wrapping handleBlogLikeStatus inside asyncHandler to handle any type of error

--- src/controllers/blogLike.controller.ts
+++ src/controllers/blogLike.controller.ts
@@ -1,31 +1,36 @@
-import { BlogLike } from '../models/blogLike';
 import { Response, Request } from 'express';
+import { asyncHandler } from './signin.controller';
+import { BlogLike } from '../models/blogLike';
+import { BlogLikeResponse } from '../types/blog.type';
 
-export const handleBlogLikeStatus = async (
-  req: Request<{ blogId: string }, { isLiked: boolean }>,
-  res: Response,
-) => {
-  const { blogId } = req.params;
-  const { isLiked } = req.body;
+export const handleBlogLikeStatus = asyncHandler(
+  async (
+    req: Request<{ blogId: string }, {}, { isLiked: boolean }>,
+    res: Response<BlogLikeResponse>,
+  ) => {
+    const { blogId } = req.params;
+    const { isLiked } = req.body;
 
-  if (!req.user) {
-    return res.status(401).json({ success: false, message: 'Unauthorized' });
-  }
-  const userId = req.user.id;
-  let result = await BlogLike.findOne({ blogId, userId });
+    if (!req.user) {
+      return res.status(401).json({ success: false, message: 'Unauthorized' });
+    }
 
-  if (result) {
-    result.isLiked = isLiked;
-    await result.save();
-  } else {
-    result = await BlogLike.create({ blogId, userId, isLiked });
-  }
+    const userId = req.user.id;
+    let result = await BlogLike.findOne({ blogId, userId });
 
-  return res.status(200).json({
-    message: isLiked ? 'Blog liked successfully' : 'Blog unliked successfully',
-    success: true,
-    data: {
-      isLiked: result.isLiked,
-    },
-  });
-};
+    if (result) {
+      result.isLiked = isLiked;
+      await result.save();
+    } else {
+      result = await BlogLike.create({ blogId, userId, isLiked });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: isLiked ? 'Blog liked successfully' : 'Blog unliked successfully',
+      data: {
+        isLiked: result.isLiked,
+      },
+    });
+  },
+);
