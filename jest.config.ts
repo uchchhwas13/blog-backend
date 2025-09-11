04c8b3b8fb245486048d20b226ca02f12b4f2f4c
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-09-11 10:56:07 +0600

    configuring jest

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+const { createDefaultPreset } = require('ts-jest');
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  testEnvironment: 'node',
+  transform: {
+    ...tsJestTransformCfg,
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  testMatch: ['**/__tests__/**/*.test.ts', '**/?(*.)+(spec|test).ts'],
+};
