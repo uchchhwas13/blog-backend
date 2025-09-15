fdb3061db87c738d0775407fa7e57ced9f060bd8
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-09-15 15:31:44 +0600

    Removing unused import

--- src/utils/fileUrlGenerator.ts
+++ src/utils/fileUrlGenerator.ts
@@ -1,4 +1,3 @@
-import { Request } from 'express';
 const BASE_URL = process.env.BASE_URL || 'http://localhost:3000';
 export const buildFileUrl = (relativePath: string): string => {
   return `${BASE_URL}${relativePath}`;
